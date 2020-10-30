97d924c413061b452cabc35a84d61b04884912ef
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-10-29 19:27:08 -0700

    updated tests

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from "react" ;
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+  
+  function transition(newMode, replace = false) { 
+    {replace && 
+      history.pop()
+    }
+    setMode(newMode)
+    setHistory(history.concat([newMode]))
+   }
+  function back() { 
+  { history.length > 1 &&
+    history.pop()
+    setHistory([...history])
+    setMode(history[history.length -1])
+  }
+}
+  return { mode, transition, back };
 }
