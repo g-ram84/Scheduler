c8527d9981b768d1167db42a85bfc5f46e33a5a7
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-11-07 13:09:12 -0800

    Bug fix adding interview.  Must select interviewer

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,6 +23,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
     setError("");
     props.onSave(name, interviewer);
   }
